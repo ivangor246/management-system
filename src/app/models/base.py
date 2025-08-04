a3beebe661db4e90f7d271b58dbc4ece535918d3
ivangor246 <ivangor246@gmail.com>
2025-08-05 00:16:07 +0300

    Create base model

---
+++ src/app/models/base.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    is_available: Mapped[bool] = mapped_column(default=True)
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
