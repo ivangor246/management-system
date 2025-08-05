07e1a6d53c58f021eb46d3bc19f7ce99cb2b2dba
ivangor246 <ivangor246@gmail.com>
2025-08-05 16:54:43 +0300

    Change base model

--- src/app/models/base.py
+++ src/app/models/base.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+from typing import Annotated
 
-from sqlalchemy import func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry
+
+str_1 = Annotated[str, 1]
+str_50 = Annotated[str, 50]
+str_100 = Annotated[str, 100]
 
 
 class Base(DeclarativeBase):
@@ -10,3 +15,11 @@ class Base(DeclarativeBase):
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    registry = registry(
+        type_annotation_map={
+            str_1: String(1),
+            str_50: String(50),
+            str_100: String(100),
+        }
+    )
