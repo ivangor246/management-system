51c756a967f8ab123a9f9b2c3b80a49047d60699
ivangor246 <ivangor246@gmail.com>
2025-09-30 01:57:05 +0300

    Change context providing

--- src/app/front/router.py
+++ src/app/front/router.py
@@ -3,27 +3,29 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 
-async def get_is_auth(request: Request) -> bool:
+async def get_context(request: Request) -> bool:
     token = request.cookies.get('access_token')
-    request.state.is_auth = bool(token)
-    return request.state.is_auth
+    request.state.context = {
+        'is_auth': bool(token),
+    }
+    return request.state.context
 
 
 templates = Jinja2Templates(directory='app/front/templates')
 
-front_router = APIRouter(dependencies=[Depends(get_is_auth)])
+front_router = APIRouter(dependencies=[Depends(get_context)])
 
 
 @front_router.get('/', response_class=HTMLResponse)
 async def home_page(request: Request):
-    return templates.TemplateResponse('home.html', {'request': request, 'is_auth': request.state.is_auth})
+    return templates.TemplateResponse('home.html', {'request': request, 'context': request.state.context})
 
 
 @front_router.get('/register', response_class=HTMLResponse)
 async def register_page(request: Request):
-    return templates.TemplateResponse('register.html', {'request': request, 'is_auth': request.state.is_auth})
+    return templates.TemplateResponse('register.html', {'request': request, 'context': request.state.context})
 
 
 @front_router.get('/login', response_class=HTMLResponse)
 async def login_page(request: Request):
-    return templates.TemplateResponse('login.html', {'request': request, 'is_auth': request.state.is_auth})
+    return templates.TemplateResponse('login.html', {'request': request, 'context': request.state.context})
